108e148000b9f9f2ea0d5d16de28219f80cff54f
veeranki2014 <veeranki2014@gmail.com>
2023-06-17 18:38:10 -0500

    roboshop_setup_shell

--- frontend.sh
+++ frontend.sh
@@ -14,7 +14,7 @@ unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 ## we need to copy config file
 # vim /etc/nginx/default.d/roboshop.conf
 echo -e "\e[34m update Nginx Reverse Proxy \e[0m"
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+cp /home/centos/roboshop_shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[34m Starting Nginx Servers \e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log

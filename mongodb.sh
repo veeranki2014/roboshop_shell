98e250a6d6c53fb2457d12a69e8264f2dbcd8be9
veeranki2014 <veeranki2014@gmail.com>
2023-06-17 12:11:14 -0500

    roboshop_setup_shell

---
+++ mongodb.sh
@@ -0,0 +1,13 @@
+echo -e "\e[34m Setup Mongodb Repo \e[0m"
+cp mongodb.repo /ect/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[34m Install MongoDB \e[0m"
+yum install mongodb-org -y &>>/tmp/roboshop.log
+
+echo -e "\e[34m Update Listen address \e[0m"
+
+##Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+
+echo -e "\e[34m Start and Enable MongoDB Service \e[0m"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl start mongod &>>/tmp/roboshop.log

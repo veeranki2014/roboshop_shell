2b9e5c29aefaf529bc3ee547c381ccf6fefc28bd
veeranki2014 <veeranki2014@gmail.com>
2023-06-17 19:04:17 -0500

    roboshop_setup_shell

--- catalogue.sh
+++ catalogue.sh
@@ -2,21 +2,45 @@ echo -e "\e[34m Setup NodeJS Repos \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
 echo -e "\e[34m Install NodeJS \e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+yum install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m Add App user \e[0m"
-useradd roboshop &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[34m Remove Old Code \e[0m"
-mkdir /app &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+rm -rf /app/* &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[34m Download App Code \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 
-echo -e "\e[34m I \e[0m"
+echo -e "\e[34m Extract the App Code \e[0m"
 cd /app
 unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
-cd /app
 
 echo -e "\e[34m Install NodeJS \e[0m"
+cd /app
 npm install &>>/tmp/roboshop.log
+
+echo -e "\e[34m  Create SystemD catalogue Service \e[0m"
+cp /home/centos/roboshop_shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+
+echo -e "\e[34m  Enable and restart catalogue \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl restart catalogue &>>/tmp/roboshop.log
+
+echo -e "\e[34m  Create SystemD catalogue Service \e[0m"
+cp /home/centos/roboshop_shell/mongodb.repo /etc/yum.repos.d/mongod.repo &>>/tmp/roboshop.log
+
+echo -e "\e[34m  Create SystemD catalogue Service \e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+
+echo -e "\e[34m  Create SystemD catalogue Service \e[0m"
+mongo --host mongodb-dev.veerankitek.com </app/schema/catalogue.js &>>/tmp/roboshop.log
+
+
+
+
+
+

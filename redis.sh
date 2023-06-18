16824a27b4ae9120296383d4d9751fef844abb77
veeranki2014 <veeranki2014@gmail.com>
2023-06-17 20:56:46 -0500

    roboshop_setup_shell

---
+++ redis.sh
@@ -0,0 +1,16 @@
+echo -e "\e[33m Setup Redis Repo \e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[33m Enable Redis Repo \e[0m"
+yum module enable redis:remi-6.2 -y
+
+echo -e "\e[33m Install Redis Repo \e[0m"
+yum install redis -y
+
+echo -e "\e[33m update Redis Listen Address \e[0m"
+#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis/conf
+
+echo -e "\e[33m Restart Redis Server \e[0m"
+systemctl enable redis
+systemctl start redis

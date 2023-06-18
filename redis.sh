97f9910f67b969d00f937627cc6a6db6a01e30f2
veeranki2014 <veeranki2014@gmail.com>
2023-06-17 21:10:58 -0500

    roboshop_setup_shell

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m update Redis Listen Address \e[0m"
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis/conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m Restart Redis Server \e[0m"
 systemctl enable redis &>>/tmp/roboshop.log

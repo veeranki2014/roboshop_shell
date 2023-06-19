ba97cdb5842b68bbbf7454f2b042125344c14bf0
veeranki2014 <veeranki2014@gmail.com>
2023-06-19 16:08:34 -0500

    roboshop_setup_shell

---
+++ payment.sh
@@ -0,0 +1,24 @@
+
+echo -e "\e[33m Install Python \e[0m"
+yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Crate App User\e[0m"
+useradd roboshop  &>>/tmp/roboshop.log
+
+echo -e "\e[33m Create App \e[0m"
+rm -rf /app* &>>/tmp/roboshop.log
+mkdir /app  &>>/tmp/roboshop.log
+
+echo -e "\e[33m Copy and Unzip App code\e[0m"
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
+cd /app
+unzip /tmp/payment.zip  &>>/tmp/roboshop.log
+
+echo -e "\e[33m install Python  \e[0m"
+cd /app
+pip3.6 install -r requirements.txt  &>>/tmp/roboshop.log
+
+echo -e "\e[33m Restart Payment Service\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable payment  &>>/tmp/roboshop.log
+systemctl start payment &>>/tmp/roboshop.log

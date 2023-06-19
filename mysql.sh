3498b941cd9cca51ebbb57a160d4c1c417974a93
veeranki2014 <veeranki2014@gmail.com>
2023-06-19 13:12:27 -0500

    roboshop_setup_shell

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,7 @@ yum module disable mysql -y  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Create Mysql Repo \e[0m"
 # cp mysql.repo
-cp /home/centos/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop_shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33m Installation MySQL\e[0m"
 yum install mysql-community-server -y &>>/tmp/roboshop.log

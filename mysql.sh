17e354f0d60f0d5e991277bacdfc4853a7162786
veeranki2014 <veeranki2014@gmail.com>
2023-06-19 13:07:24 -0500

    roboshop_setup_shell

---
+++ mysql.sh
@@ -0,0 +1,17 @@
+
+echo -e "\e[33m Disable MySQL Module\e[0m"
+yum module disable mysql -y  &>>/tmp/roboshop.log
+
+echo -e "\e[33m Create Mysql Repo \e[0m"
+# cp mysql.repo
+cp /home/centos/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33m Installation MySQL\e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Set Root password\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
+
+echo -e "\e[33m Restart the mysql\e[0m"
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl restart mysqld &>>/tmp/roboshop.log

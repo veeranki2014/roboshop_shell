e5211586b5de3af50dc72a3c80049e6750c9c316
veeranki2014 <veeranki2014@gmail.com>
2023-06-19 19:58:31 -0500

    roboshop_setup_shell

--- catalogue.sh
+++ catalogue.sh
@@ -2,15 +2,8 @@ source common.sh
 component=catalogue
 
 nodejs
+mongo_schema_setup
 
-echo -e "${colour}  Create SystemD $catalogue Service ${nocolour}"
-cp /home/centos/roboshop_shell/mongodb.repo /etc/yum.repos.d/mongod.repo &>>${log_file}
-
-echo -e "${colour}  Create SystemD $catalogue Service ${nocolour}"
-yum install mongodb-org-shell -y &>>${log_file} &>>${log_file}
-
-echo -e "${colour}  Create SystemD $catalogue Service ${nocolour}"
-mongo --host mongodb-dev.veerankitek.com <${app_path}/schema/$catalogue.js &>>${log_file}
 
 
 
--- common.sh
+++ common.sh
@@ -3,13 +3,7 @@ nocolour="${\e[0m]}"
 log_file=/tmp/roboshop.log
 app_path=/app
 
-nodejs(){
-  echo -e "${colour} Setup NodeJS Repos ${nocolour}"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-
-  echo -e "${colour} Install NodeJS ${nocolour}"
-  yum install nodejs -y &>>${log_file}
-
+app_presetup(){
   echo -e "${colour} Add App user ${nocolour}"
   useradd roboshop &>>${log_file}
 
@@ -22,19 +16,34 @@ nodejs(){
 
   echo -e "${colour} Extract the App Code ${nocolour}"
   cd ${app_path}
-  unzip /tmp/$catalogue.zip &>>${log_file}
+  unzip /tmp/${component}.zip &>>${log_file}
+
+}
+
+systemd_setup(){
+  echo -e "${colour}  Create SystemD ${component} Service ${nocolour}"
+  cp /home/centos/roboshop_shell/$catalogue.service /etc/systemd/system/${component}.service &>>${log_file}
+
+  echo -e "${colour}  Enable and restart ${component} ${nocolour}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable ${component} &>>${log_file}
+  systemctl restart ${component} &>>${log_file}
+}
+
+nodejs(){
+  echo -e "${colour} Setup NodeJS Repos ${nocolour}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
   echo -e "${colour} Install NodeJS ${nocolour}"
+  yum install nodejs -y &>>${log_file}
+
+ app_presetup
+
+  echo -e "${colour} Install NodeJS Dependencies${nocolour}"
   cd ${app_path}
   npm install &>>${log_file}
 
-  echo -e "${colour}  Create SystemD $catalogue Service ${nocolour}"
-  cp /home/centos/roboshop_shell/$catalogue.service /etc/systemd/system/$catalogue.service &>>${log_file}
-
-  echo -e "${colour}  Enable and restart $catalogue ${nocolour}"
-  systemctl daemon-reload &>>${log_file}
-  systemctl enable $catalogue &>>${log_file}
-  systemctl restart $catalogue &>>${log_file}
+  systemd_setup
 
 }
 
@@ -47,4 +56,45 @@ mongo_schema_setup(){
 
   echo -e "\e[33m  Create SystemD catalogue Service \e[0m"
   mongo --host mongodb-dev.veerankitek.com </app/schema/user.js &>>/tmp/roboshop.log
+}
+
+maven(){
+  echo -e "${colour}  Install Maven ${nocolour}"
+  yum install maven -y  &>>${log_file}
+
+  app_presetup
+
+  echo -e "${colour} Build the code and target  ${nocolour}"
+  cd ${app_path}
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+
+  echo -e "${colour} Copy ${component} service  ${nocolour}"
+  ##copy ${component} service
+  cp /home/centos/roboshop_shell/${component}.service /etc/systemd/system/${component}.service
+
+  echo -e "${colour} Install MYSQL  ${nocolour}"
+  yum install mysql -y  &>>${log_file}
+
+  echo -e "${colour}  Load Mysql schema ${nocolour}"
+  mysql -h mysql-dev.veerankitek.com -uroot -pRoboShop@1 < ${app_path}/schema/${component}.sql  &>>${log_file}
+
+  echo -e "${colour} Restart ${component} services again  ${nocolour}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable ${component} &>>${log_file}
+  systemctl restart ${component}  &>>${log_file}
+}
+
+python(){
+
+  echo -e "${colour} Install Python ${nocolour}"
+  yum install python36 gcc python3-devel -y &>>${log_file}
+
+  app_presetup
+
+  echo -e "${colour} install Python  ${nocolour}"
+  cd /app
+  pip3.6 install -r requirements.txt  &>>${log_file}
+
+  systemd_setup
 }
